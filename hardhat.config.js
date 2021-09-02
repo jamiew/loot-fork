6951d559c27d17186f5f67e427d67bb0d5dd26c4
Jamie Wilkinson <jamie@jamiedubs.com>
2021-09-01 22:02:40 -0400

    Refactor hardhat config to my liking

--- hardhat.config.js
+++ hardhat.config.js
@@ -27,11 +27,10 @@ module.exports = {
     hardhat: {
       initialBaseFeePerGas: 0, // workaround from https://github.com/sc-forks/solidity-coverage/issues/652#issuecomment-896330136 . Remove when that issue is closed.
     },
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
+    rinkeby: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [process.env.WALLET_PRIVATE_KEY]
+    }
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
